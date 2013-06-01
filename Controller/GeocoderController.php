3adf7aff43dc3283d3e9c8fc4d33574aff5f9e01
Alessio <io.alessio@gmail.com>
2013-06-01 22:25:13 +0300

    Update GeocoderController.php

--- Controller/GeocoderController.php
+++ Controller/GeocoderController.php
@@ -3,26 +3,19 @@
 namespace Io\GeocoderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Component\HttpFoundation\Response;
 
 
 class GeocoderController extends Controller
 {
-    /**
-     * @todo Adding cache information
-     * @param type $address
-     */
     public function indexAction()
     {        
         $request = $this->getRequest();
-        $locale = $request->getLocale();        
+        $locale = $request->getLocale();
         $address = $request->query->get('address');
         
         $remote_url = "http://maps.googleapis.com/maps/api/geocode/json?address=" . $address . "&sensor=false&hl=".$locale;
-        
-        
+
         $response = $this->curl($remote_url);
         $response->setPublic();
         $response->setExpires(new \DateTime('tomorrow'));
